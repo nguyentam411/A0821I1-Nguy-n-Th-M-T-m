0283d4f0166a08f5e6216788a78f16079f9690b2
unknown <tamnguyen.04112003@gmail.com>
2021-11-01 21:10:05 +0700

    nop bai

---
+++ module2/src/module2/_3_mang_phuong_thuc_trong_java/exercise/dem_ki_tu_giong.java
@@ -0,0 +1,22 @@
+package module2._3_mang_phuong_thuc_trong_java.exercise;
+
+import java.util.Scanner;
+
+public class dem_ki_tu_giong {
+    public static void main(String[] args) {
+        String str;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập chuỗi: ");
+        str = sc.nextLine();
+        System.out.println("Nhập kí tự cần kiểm tra: ");
+        char c;
+        c = sc.next().charAt(0);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        System.out.println("Số lần xuất hiện của kí tự là "+count);
+    }
+}
