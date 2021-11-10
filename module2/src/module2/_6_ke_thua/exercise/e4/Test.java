ef2029b361069c12f6339a57252b25040919756d
unknown <tamnguyen.04112003@gmail.com>
2021-11-10 23:34:03 +0700

    nop bai

---
+++ module2/src/module2/_6_ke_thua/exercise/e4/Test.java
@@ -0,0 +1,8 @@
+package module2._6_ke_thua.exercise.e4;
+
+public class Test {
+    public static void main(String[] args) {
+        Triangle triangle=new Triangle(3,4,5);
+        System.out.println(triangle);
+    }
+}
