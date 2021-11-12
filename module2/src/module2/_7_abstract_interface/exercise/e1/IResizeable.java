c1dd86695dcc1f8dc23d11aad6aeb64bebae6970
unknown <tamnguyen.04112003@gmail.com>
2021-11-12 17:51:36 +0700

    nop bai

---
+++ module2/src/module2/_7_abstract_interface/exercise/e1/IResizeable.java
@@ -0,0 +1,5 @@
+package module2._7_abstract_interface.exercise.e1;
+
+public interface IResizeable {
+    void resize(double percent);
+}
---
+++ module2/src/module2/_7_abstract_interface/exercise/e1/Test.java
@@ -0,0 +1,19 @@
+package module2._7_abstract_interface.exercise.e1;
+
+public class Test {
+    public static void main(String[] args) {
+        IResizeable[] resizeable=new IResizeable[3];
+        resizeable[0]=new Circle(4);
+        resizeable[1]=new Rectangle(2,6,"blue",false);
+        resizeable[2]=new Square(4,"red",false);
+        for (IResizeable result:resizeable){
+            if (result instanceof  Circle){
+                result.resize(50);
+                System.out.println(result);
+            }else if (result instanceof Rectangle){
+                result.resize(50);
+                System.out.println(result);
+            }
+        }
+    }
+}
