6458e3a55a19c1403cd301574489f1379348616c
unknown <tamnguyen.04112003@gmail.com>
2021-12-30 19:21:32 +0700

    nop bai

---
+++ module3/2. Thiết kế và tạo CSDL/Bài tập/Quản lý bán hàng.sql
@@ -0,0 +1,27 @@
+create database QuanLyBanHang;
+use QuanLyBanHang;
+create table Customer(
+cID int auto_increment primary key,
+cName varchar(20),
+cAge int
+);
+create table `Order` (
+oID int auto_increment primary key,
+cID int,
+oDate date,
+oTotalPrice int,
+foreign key(cID) references Customer(cID)
+);
+create table Product(
+pID int auto_increment primary key,
+pName varchar(20),
+pPrice int
+);
+create table OrderDetail(
+oID int,
+pID int,
+odQTY int,
+primary key(oID, pID),
+foreign key (oID) references `Order`(oID),
+foreign key (pID) references Product(pID)
+);
