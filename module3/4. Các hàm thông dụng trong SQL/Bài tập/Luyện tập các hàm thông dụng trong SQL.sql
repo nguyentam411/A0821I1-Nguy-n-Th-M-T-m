1c7bb93e1b66211721021d02e7a4ba98f5f73772
unknown <tamnguyen.04112003@gmail.com>
2022-01-06 22:26:35 +0700

    nop bai

--- module3/4. Các hàm thông dụng trong SQL/Bài tập/Luyện tập các hàm thông dụng trong SQL.sql
+++ module3/4. Các hàm thông dụng trong SQL/Bài tập/Luyện tập các hàm thông dụng trong SQL.sql
@@ -1,9 +1,12 @@
 use quanlysinhvien;
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất. 
 select * from subject
 where Credit >= all(select Credit from subject);
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
 select sub.SubID,sub.SubName,m.Mark from subject as sub
 join mark as m on m.SubID=sub.SubID
 having Mark>=any(select max(Mark) from mark group by SubID ); 
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 select s.StudentID,s.StudentName,avg(mark),m.mark from student as s
 join mark as m on m.StudentID=s.StudentID
 group by s.StudentID
