68c8f06069d9a81a0e6ce823d2bc29ad0b3cd4c0
unknown <tamnguyen.04112003@gmail.com>
2022-01-09 00:11:52 +0700

    nop bai

--- module3/5. SQL Index, View, Stored Prodeced/Bài tập/View, Index, Store Procedure.sql
+++ module3/5. SQL Index, View, Stored Prodeced/Bài tập/View, Index, Store Procedure.sql
@@ -13,8 +13,10 @@ insert into products values
 (1,'A123','pen',1000,5,'blue',1),
 (2,'B123','book',5000,10,'green',0),
 (3,'A124','ruler',3000,2,'red',1);
+-- Tạo Unique Index trên bảng Products (sử dụng cột productCode để tạo chỉ mục)
  create unique index unique_index
  on products(productCode);
+--  Tạo Composite Index trên bảng Products (sử dụng 2 cột productName và productPrice)
  create index composite_index
  on products(productName,productPrice);
  drop index unique_index on products;
@@ -23,26 +25,28 @@ insert into products values
  where productCode='B123';
  select * from products 
 where productName='pen' or productPrice=5000;
+-- Sử dụng câu lệnh EXPLAIN để biết được câu lệnh SQL của bạn thực thi như nào
  explain select * from products 
  where productCode='B123';
 explain select * from products 
 where productName='pen' or productPrice=5000;
- drop TABLE PRODUCTS;
+-- Tạo view lấy về các thông tin: productCode, productName, productPrice, productStatus từ bảng products.
  create view product_info as
  select productCode,productName,productPrice,productStatus from products;
- insert into product_info value
- ('C123','pencil',4000,1);
+--  Tiến hành sửa đổi view
  update product_info 
  set productPrice=6000
  where productName='pen';
+--  Tiến hành xoá view
  drop view product_info;
+--  Tạo store procedure lấy tất cả thông tin của tất cả các sản phẩm trong bảng product
  DELIMITER //
  create procedure getAll()
  begin 
  select * from products;
  end // 
  call getAll();
- 
+--  Tạo store procedure thêm một sản phẩm mới
 DELIMITER //
  create procedure addProduct(
 in id int,
@@ -57,6 +61,7 @@ in `status` bit)
  (id,`code`,`name`,price,amount,`description`,`status`);
  end // 
  call addProduct(5,'D123','bag',20000,4,'BLUE',1);
+--  Tạo store procedure sửa thông tin sản phẩm theo id
 DELIMITER //
  create procedure updateProduct(
  in id_update int,
@@ -78,6 +83,7 @@ in `status` bit)
  where Id=id_update;
  end // 
  call updateProduct(3,'bag',1000,5,'black',1)
+--  Tạo store procedure xoá sản phẩm theo id
  DELIMITER //
  create procedure deleteProduct(IN id_delete int)
  begin 
