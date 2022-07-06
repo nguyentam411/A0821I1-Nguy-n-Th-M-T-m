cf521609c6c16826bbd57eba6afb7e961e2947dc
unknown <tamnguyen.04112003@gmail.com>
2022-07-06 14:28:07 +0700

    up

---
+++ module5/6. Angular Form/exercise/angular-login-form/angular-login-form/src/app/component/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+ loginForm=new FormGroup({
+   email:new FormControl("",[Validators.required,Validators.email]),
+   password:new FormControl("",[Validators.required,Validators.minLength(6)])
+ })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value)
+  }
+
+  get email() {
+    return this.loginForm.get('email')
+  }
+
+  get password() {
+    return this.loginForm.get('password')
+  }
+}
--- module5/6. Angular Form/exercise/angular-register/angular-register/src/app/component/register/register.component.ts
+++ module5/6. Angular Form/exercise/angular-register/angular-register/src/app/component/register/register.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {Register} from "./register";
+import {AbstractControl,FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,17 +7,20 @@ import {Register} from "./register";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registers: Register[] = []
   registerForm = new FormGroup({
-    email: new FormControl("",[Validators.required,Validators.email]),
-    password: new FormControl("",[Validators.required,Validators.minLength(6)]),
-    confirmPassword: new FormControl("",[Validators.required,Validators.minLength(6)]),
-    country: new FormControl("",[Validators.required]),
-    age: new FormControl("",[Validators.required,Validators.min(18)]),
-    gender: new FormControl("",[Validators.required]),
-    phone: new FormControl("",[Validators.required,Validators.pattern( /^\+84\d{9,10}$/)])
-  })
-  contactForm: any;
+      email: new FormControl("", [Validators.required, Validators.email]),
+    pw:new FormGroup({
+      password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl("", [Validators.required, Validators.minLength(6)]),
+
+    },this.comparePassword),
+      country: new FormControl("", [Validators.required]),
+      age: new FormControl("", [Validators.required, Validators.min(18)]),
+      gender: new FormControl("", [Validators.required]),
+      phone: new FormControl("", [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)])
+    },
+    // {validators: this.mustMatch("password","confirmPassword"}
+  )
 
   constructor() {
   }
@@ -27,8 +29,40 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit() {
-    const value = this.registerForm.value;
-    this.registers.push(value);
-    this.registerForm.reset();
+    console.log(this.registerForm.value)
+  }
+
+  get email() {
+    return this.registerForm.get('email')
+  }
+
+  get password() {
+    return this.registerForm.get('pw').get('password');
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('pw').get('confirmPassword');
+  }
+
+  get country() {
+    return this.registerForm.get('country')
+  }
+
+  get age() {
+    return this.registerForm.get('age')
+  }
+
+  get gender() {
+    return this.registerForm.get('gender')
+  }
+
+  get phone() {
+    return this.registerForm.get('phone')
+  }
+  comparePassword(c: AbstractControl) {
+    const v = c.value;
+    return (v.password === v.confirmPassword) ? null : {
+      passwordNotMatch: true
+    };
   }
 }
